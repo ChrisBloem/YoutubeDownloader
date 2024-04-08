56b8a162b0374422f7d6edabbd4d8d630145acb1
ChrisBloem <chrisbloem1@hotmail.com>
2024-04-08 11:57:02 +0200

    First commit

--- main.py
+++ main.py
@@ -1,36 +1,27 @@
 from pytube import YouTube 
-import os
-import math
-
-# Function to handle the progress
-def progress_function(stream, chunk, bytes_remaining):
-    total_size = stream.filesize
-    bytes_downloaded = total_size - bytes_remaining 
-    percentage_of_completion = math.floor((bytes_downloaded / total_size) * 100)
-    print(f'Downloading: {percentage_of_completion}%', end='\r')
 
 # where to save 
-#SAVE_PATH = DriveFolderPathToWhereYouWantToStoreVideos #Change path to your location (windows)
-SAVE_PATH = "/Users/yourusername/Folder/To/Download/To"  # Update this path (MAC)
+SAVE_PATH = "C:/Users/*UserName*/Downloads" #Change path to your location (windows)
+#SAVE_PATH = "/Users/yourusername/Folder/To/Download/To"  # Update this path (MAC)
 
 
-# link of the video to be downloaded
-link = "YOUTUBE_VIDEO_LINK_HERE"  # Add youtube video link here
+# link of the video to be downloaded 
+link = "YOUTUBE_LINK_HERE" #Add youtube video link here
 
-try:
-    # object creation using YouTube
-    yt = YouTube(link, on_progress_callback=progress_function)
-except:
-    # to handle exception
-    print("Connection Error")
+try: 
+    print("Download started...")
+    # object creation using YouTube 
+    yt = YouTube(link) 
+except: 
+    #to handle exception 
+    print("Connection Error") 
 
-# Get the video with the highest resolution
+# get the video with the highest resolution
 d_video = yt.streams.get_highest_resolution()
 
-try:
-    # downloading the video
-    print("Downloading...")
+try: 
+    # downloading the video 
     d_video.download(output_path=SAVE_PATH)
-    print('\nVideo downloaded successfully!')
-except Exception as e:
-    print("Some Error:", e)
+    print('Video downloaded successfully!')
+except: 
+    print("Some Error!")
